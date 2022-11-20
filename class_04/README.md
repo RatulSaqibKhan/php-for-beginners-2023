8e47d26507e211b614d7c1a451348924c8f73c63
RatulSaqibKhan <ratulkhan.jhenidah@gmail.com>
2022-11-20 11:57:01 +0600

    Class 4 - PHP Variable

---
+++ class_04/README.md
@@ -0,0 +1,12 @@
+# PHP Variables
+
+## ToDo List
+  1. Create a variable containing the greeting "Hello"
+  2. Print the variable to show the message "Hello, World!"
+
+
+## To run server
+ - php -S localhost:8113
+
+## Homework
+  - Now that $greeting is a variable, do the same for the noun ("World"). Then play around with changing the variable values to generate different sentences.
---
+++ class_04/demo/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP Variable</title>
+</head>
+<body>
+  
+  <h1>
+    <?php
+        $greeting = "Hello";
+        // use concatination operator .
+        // echo $greetins . ', World';
+        
+        // use variable inside string
+        // echo "$greeting, World";
+    ?>
+    <?php // Alternative of echo ?>
+    <?= "$greeting, World" ?>
+  </h1>
+
+</body>
+</html>
