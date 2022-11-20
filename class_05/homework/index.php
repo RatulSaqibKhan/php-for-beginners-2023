e4fdbc26d625ddb81ec14e0c98c5b88d2145816c
RatulSaqibKhan <ratulkhan.jhenidah@gmail.com>
2022-11-20 12:45:13 +0600

    Class 05 - Homework

---
+++ class_05/homework/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Class 05 - Homework</title>
+</head>
+<body>
+    <?php
+        $message = "Hello, World!";
+    ?>
+    <h1>
+        <?= $message ?>
+    </h1>
+</body>
+</html>
