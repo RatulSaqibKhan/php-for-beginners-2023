321b0c6e2bb0feaba4828f98891007be6bdc7491
RatulSaqibKhan <ratulkhan.jhenidah@gmail.com>
2022-11-20 11:19:27 +0600

    Class 1 - Homework

---
+++ class_03/homework/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>First PHP Tag</title>
+</head>
+<body>
+  
+  <h1>
+    <?php
+        echo "Hello World";
+    ?>
+  </h1>
+
+  <p>
+    <?php
+        echo "I am Md. Nazmus Saqib Khan! A learner of Web Development.";
+    ?>
+  </p>
+
+</body>
+</html>
