9da6eb120a3664c931be3cfbb7d5a56c8008c3d1
RatulSaqibKhan <ratulkhan.jhenidah@gmail.com>
2022-11-26 20:16:08 +0600

    Class 12 - PHP Partials

---
+++ class_12/README.md
@@ -0,0 +1,4 @@
+# PHP Partials
+
+## ToDo
+  1. In the previous episode, we begrudgingly duplicated the same HTML for every PHP view. Let's fix that now by reaching for PHP partials.
---
+++ class_12/demo/views/contact.view.php
@@ -0,0 +1,13 @@
+<?php require "partials/header.view.php"; ?>
+<?php require "partials/nav.view.php"; ?>
+<?php require "partials/banner.view.php"; ?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <!-- Replace with your content -->
+        <p>Contact page!</p>
+        <!-- /End replace -->
+    </div>
+</main>
+
+<?php require "partials/footer.view.php"; ?>
